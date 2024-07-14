d446d0d83963e0c4b834bb62b56425564960ac9b
levin <levin@sevastopol.softlab.ru>
2024-07-14 20:51:05 +0300

    Редактирование пустых записей вместо создания новых. Очистка вместо удаления

--- repositories/scheduleRepository.js
+++ repositories/scheduleRepository.js
@@ -4,23 +4,47 @@ import {asyncAirtable} from "../common/airtable.js";
 const TABLE = 'Расписание';
 const VIEW = 'Расписание main';
 
-// Таблица Расписание содержит только текущее расписание на 1 тур, поэтому всегда вычитываем и удаляем все содержимое таблицы
+// Таблица Расписание содержит только текущее расписание на один тур, поэтому всегда вычитываем и очищаем все содержимое таблицы
 export default class ScheduleRepository {
     getAll = async () => asyncAirtable.select(TABLE, {
         view: VIEW,
     }).then(records => records.map(record => minify(record)));
 
-    saveList = async records => Promise.all(chunkArray(records)
+    saveList = async records => process.env.UPDATE_EMPTY_RECORDS_WHEN_CREATE
+        ? this.#workaroundSaveList(records)
+        : this.#normalSaveList(records);
+
+    #normalSaveList = records => Promise.all(chunkArray(records)
         .map(chunk => asyncAirtable.bulkCreate(TABLE, chunk.map(record => ({
             "Распределение команд": record.distributions,
         })))));
 
-    removeList = async records => Promise.all(
-        chunkArray(records).map(chunk => asyncAirtable.bulkDelete(TABLE, chunk)));
+    getEmptyList = async count =>
+        asyncAirtable.select(TABLE, {
+            view: VIEW,
+            filterByFormula: "{Турнир} = ''",
+            maxRecords: count,
+        }).then(records => records.map(record => minifyEmpty(record)));
+
+    #workaroundSaveList = records =>
+        this.getEmptyList(records.length)
+            .then(emptyRecords => records.map((record, index) => ({
+                ...record,
+                id: emptyRecords[index].id
+            }))) //объединяем записи с идентификаторами
+            .then(records => this.updateList(records));
+
+    updateList = async records => Promise.all(chunkArray(records)
+        .map(chunk => asyncAirtable.bulkUpdate(TABLE, chunk.map(record => ({
+            id: record.id,
+            fields: {
+                "Распределение команд": record.distributions,
+            }
+        })))));
 
     removeAll = async () => this.getAll()
-        .then(records => records.map(r => r.id))
-        .then(records => this.removeList(records));
+        .then(records => records.map(r => ({...r, distributions: null})))
+        .then(records => this.updateList(records));
 }
 
 export const minify = record => ({
@@ -31,4 +55,8 @@ export const minify = record => ({
     group: record.fields['Группа'] && record.fields['Группа'][0],
     tour: !record.fields['Тур'] ? 0 : parseInt(record.fields['Тур']),
     text: record.fields['Текст'],
+});
+
+const minifyEmpty = record => ({
+    id: record.id,
 });
--- repositories/tournamentOutcomesRepository.js
+++ repositories/tournamentOutcomesRepository.js
@@ -15,6 +15,13 @@ export default class TournamentOutcomesRepository {
         filterByFormula: '{Турнир в процессе}=1'
     }).then(records => records.map(record => minify(record)));
 
+    getEmptyList = async count =>
+        asyncAirtable.select(TABLE, {
+            view: VIEW,
+            filterByFormula: "{Команда} = ''",
+            maxRecords: count,
+        }).then(records => records.map(record => minifyEmpty(record)));
+
     updateRatingList = async records => Promise.all(chunkArray(records)
         .map(chunk => asyncAirtable.bulkUpdate(TABLE, chunk.map(record => ({
             id: record.id,
@@ -24,11 +31,32 @@ export default class TournamentOutcomesRepository {
             }
         })))));
 
-    createList = async records => chunkArray(records)
+    createList = async records => process.env.UPDATE_EMPTY_RECORDS_WHEN_CREATE
+        ? this.#workaroundCreateList(records)
+        : this.#normalCreateList(records);
+
+    #normalCreateList = records => chunkArray(records)
         .map(chunk => asyncAirtable.bulkCreate(TABLE, chunk.map(record => ({
             "Турниры": [record.tournamentId],
             "Команды": [record.teamId],
         }))));
+
+    #workaroundCreateList = records =>
+        this.getEmptyList(records.length)
+            .then(emptyRecords => records.map((record, index) => ({
+                ...record,
+                id: emptyRecords[index].id
+            }))) //объединяем записи с идентификаторами
+            .then(records => this.updateList(records));
+
+    updateList = async records => Promise.all(chunkArray(records)
+        .map(chunk => asyncAirtable.bulkUpdate(TABLE, chunk.map(record => ({
+            id: record.id,
+            fields: {
+                "Турниры": [record.tournamentId],
+                "Команды": [record.teamId],
+            }
+        })))));
 }
 
 export const minify = record => ({
@@ -37,4 +65,8 @@ export const minify = record => ({
     teamId: record.fields['Команды'][0],
     teamName: record.fields['Команда'][0],
     place: record.fields['Результат (место)'],
+});
+
+const minifyEmpty = record => ({
+    id: record.id,
 });
